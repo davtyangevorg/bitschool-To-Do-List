0da2b3e57ebe6ec0b22b043497207df4010ba6c5
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2020-12-15 13:08:02 +0400

    I created the base of the site

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            To-Do-List © 2020
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Section/section.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './section.css'
+
+const Section = () => {
+    return (
+        <section className='section'>
+            Section
+        </section>
+    )
+}
+
+export default Section
