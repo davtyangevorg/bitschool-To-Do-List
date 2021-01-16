2296d4dc7addffe421458cae6f6a30a151e2d9ef
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2021-01-16 14:49:19 +0400

    create Tasks component

--- src/components/ToDoSection/ToDoListFoundation/toDoListFoundation.jsx
+++ src/components/ToDoSection/ToDoListFoundation/toDoListFoundation.jsx
@@ -1,16 +1,10 @@
-import React, { Component, useContext } from 'react'
+import React, { Component } from 'react'
 
 import { Row, Col } from 'react-bootstrap'
 import styles from './toDoListFoundation.module.scss'
 
-import taskNote from '../../../images/taskNote.svg'
-import { FaPlus } from 'react-icons/fa'
-
 import AddTaskFormModal from '../AddTaskForm/addTaskFormModal.jsx'
-import TasksGrid from '../TasksGrid/tasksGrid.jsx'
-import TasksList from '../TasksList/tasksList.jsx'
-
-import { ListOrGridSwitchContext, AddTaskModalContext } from '../../../context.js'
+import Tasks from '../Tasks/tasks.jsx'
 
 class ToDoList extends Component {
 
@@ -74,36 +68,4 @@ class ToDoList extends Component {
 }
 export default ToDoList
 
-const Tasks = ({ tasks, selectedTasksIds, deleteTask, togleSelectTask }) => {
 
-    const switchName = useContext(ListOrGridSwitchContext)
-    const { setIsShowAddTaskFormModal } = useContext(AddTaskModalContext)
-
-    return (
-        <>
-            {tasks.length
-                ? switchName === 'list'
-                    ? <TasksList
-                        tasks={tasks}
-                        deleteTask={deleteTask}
-                    />
-                    : <TasksGrid
-                        tasks={tasks}
-                        selectedTasksIds={selectedTasksIds}
-                        deleteTask={deleteTask}
-                        togleSelectTask={togleSelectTask}
-                    />
-                : <Col>
-                    <div className={styles.notTasks}>
-                        <img alt='taskNote' src={taskNote}></img>
-                        <h3>Create your first task</h3>
-                        <p>You do not have any tasks yet</p>
-                        <button onClick={() => { setIsShowAddTaskFormModal(true) }} >
-                            <FaPlus size='1rem' />
-                        </button>
-                    </div>
-                </Col>
-            }
-        </>
-    )
-}
